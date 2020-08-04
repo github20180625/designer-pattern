03253959a3309447821ea19b51159147135f9a91
shengjiangtao <shengjiangtao@zy1111.com>
2020-08-04 17:08:22 +0800

    edit box

--- src/main/javax/com/sheng/designer/creation/single/inner/InnerClassSingle.java
+++ src/main/javax/com/sheng/designer/creation/single/inner/InnerClassSingle.java
@@ -6,13 +6,58 @@ package javax.com.sheng.designer.creation.single.inner;
  */
 public class InnerClassSingle {
 
+//	private int x = 0;
+
 	private InnerClassSingle() {}
 
+	static {
+		System.out.println("加载外部类");
+	}
+
 	private static class InnerClass {
+
 		static InnerClassSingle instance = new InnerClassSingle();
+
+//		int x = 1;
+
+//		void methodInFirstLevel(int x) {
+//			System.out.println("x = " + x);
+//			System.out.println("this.x = " + this.x);
+//			System.out.println("InnerClassSingle.this.x = " + InnerClassSingle.this.x);
+//		}
+
+//		InnerClass() {
+//			System.out.println(this);
+//			System.out.println("加载InnerClass");
+//		}
 	}
 
 	public static InnerClassSingle getInstance() {
-		return InnerClass.instance;
+		return InnerClassSingle.InnerClass.instance;
+	}
+
+	public static void main(String[] args) {
+//		InnerClassSingle innerClassSingle = new InnerClassSingle();
+//		System.out.println(innerClassSingle);
+//		InnerClass innerClass = innerClassSingle.new InnerClass();
+//		innerClass.methodInFirstLevel(3);
+//		InnerClassSingle single = getInstance();
+//		System.out.println(single);
+//		InnerClassSingle innerSingle = InnerClass.instance;
+//		System.out.println(innerSingle);
+
+//		InnerClassSingle single = InnerClassSingle.getInstance();
+//		System.out.println(Thread.currentThread().getName() + "::" + single);
+
+		InnerClassSingle single1 = InnerClassSingle.getInstance();
+		System.out.println(Thread.currentThread().getName() + "::" + single1);
+
+		for (int i = 0; i < 10; i++) {
+			Thread t = new Thread(() -> System.out.println(Thread.currentThread().getName() + "::" + InnerClassSingle.getInstance()));
+			t.start();
+		}
+
+		InnerClassSingle single2 = InnerClassSingle.getInstance();
+		System.out.println(Thread.currentThread().getName() + "::" + single2);
 	}
 }
